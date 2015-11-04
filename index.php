cee3e7a2a4c4d13d28e9931b209c462004f7f0dc
syed jawwad alam <jawwad_alam2002@hotmail.com>
2015-11-04 09:59:31 +0500

    first test

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<?php
+/**
+ * Define MyClass sdsds
+ */
+class MyClass
+{
+    public $public = 'Public';
+    protected static  $protected = 'Protected';
+    private static $private = 'Private';
+
+}
+
+//$obj = new MyClass();
+//echo $obj->public; // Works
+//echo $obj->protected; // Fatal Error
+//echo $obj->private; // Fatal Error
+//$obj->printHello(); // Shows Public, Protected and Private
+
+
+/**
+ * Define MyClass2
+ */
+ 
+class MyClass2 extends MyClass
+{
+	public $hello;
+	protected static $protected="this class";
+	
+	public function MyClass2(){
+		$this->hello=parent::$protected;
+	}
+	public function printHello(){
+		echo $this->hello;
+	}
+}
+
+$obj2 = new MyClass2();
+$obj2->printHello(); // Shows Public, Protected2, Undefined
+
+?>
